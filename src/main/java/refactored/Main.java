c86ea78ccb9de9f2448121ba81d73031d5396526
Seby7890 <sebi.haraszthy@gmail.com>
2022-10-01 10:42:16 +0300

    new code

--- src/main/java/refactored/Main.java
+++ src/main/java/refactored/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         EncodingModule encodingModule = new EncodingModule();
+
         encodingModule.encodeFromFileToFile();
         encodingModule.encodeFromFileToDatabase();
     }
