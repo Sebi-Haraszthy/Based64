90f0edd1fe90819f70bb4d9ea0bfa25517a893d3
Seby7890 <sebi.haraszthy@gmail.com>
2022-10-01 11:16:27 +0300

    new code

--- src/main/java/refactored/Main.java
+++ src/main/java/refactored/Main.java
@@ -3,15 +3,24 @@ package refactored;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         MyFileWriter myFileWriter = new MyFileWriter("fileEncrypted.txt");
         MyDatabaseWriter myDatabaseWriter = new MyDatabaseWriter();
         MyFileReader myFileReader = new MyFileReader("src/main/java/refactored/fileToBeEncrypted.txt");
-
         EncodingModule encodingModule = new EncodingModule(myFileReader, myFileWriter);
-        encodingModule.encode();
+
+        try {
+            encodingModule.encode();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
 
         encodingModule = new EncodingModule(myFileReader, myDatabaseWriter);
-        encodingModule.encode();
+
+        try {
+            encodingModule.encode();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
